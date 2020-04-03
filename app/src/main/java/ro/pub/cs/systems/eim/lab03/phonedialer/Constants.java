ce98be4510b5daf75ce26dff45df3febfade36fa
Stimabilul Baros <stimabilul.baros@gmail.com>
2020-04-03 19:39:32 +0300

    implemented tasks for laboratory 03

---
+++ app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/Constants.java
@@ -0,0 +1,20 @@
+package ro.pub.cs.systems.eim.lab03.phonedialer;
+
+public interface Constants {
+
+    int buttonIds[] = {
+            R.id.number_0_button,
+            R.id.number_1_button,
+            R.id.number_2_button,
+            R.id.number_3_button,
+            R.id.number_4_button,
+            R.id.number_5_button,
+            R.id.number_6_button,
+            R.id.number_7_button,
+            R.id.number_8_button,
+            R.id.number_9_button,
+            R.id.star_button,
+            R.id.pound_button
+    };
+    int PERMISSION_REQUEST_CALL_PHONE = 1;
+}
